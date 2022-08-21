ca5a47353fd1de42719ba5fb28091e6b5753c09f
Allen <allenyu0708@gmail.com>
2022-08-21 22:53:27 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import { setupLayouts } from "virtual:generated-layouts"
 import generatedRoutes from "virtual:generated-pages"
 
