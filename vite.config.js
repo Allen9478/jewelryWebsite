9bfebc223b9db204264a22b57593ea8af96737f7
Allen <allenyu0708@gmail.com>
2022-08-22 11:57:41 +0800

    try

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@ import AutoImport from "unplugin-auto-import/vite"
 import path from 'path'
 import Components from "unplugin-vue-components/vite"
 import Pages from 'vite-plugin-pages'
-import Layouts from 'vite-plugin-vue-layouts';
+import Layouts from 'vite-plugin-vue-layouts'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'jewelryWebsite',
+  base:'/jewelryWebsite/',
   plugins: [
     vue(),
     Pages(),
