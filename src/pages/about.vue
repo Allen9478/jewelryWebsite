a991ed9698141b036a527c3d5376667083df8efd
Allen <allenyu0708@gmail.com>
2022-08-23 23:08:42 +0800

    justtest

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,7 +1,9 @@
 <template>
-  <div class="about">
+  <!-- <div class="about">
     <h1>This is an about page</h1>
-  </div>
+  </div> -->
+  <button type="button" class="btn btn-primary">Primary</button>
+
 </template>
 
 <style>
