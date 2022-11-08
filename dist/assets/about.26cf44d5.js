6c750df8388ed941d747788059f9ec3bd332f071
Allen <allenyu0708@gmail.com>
2022-11-08 16:12:05 +0800

    menubarup

---
+++ dist/assets/about.26cf44d5.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as n}from"./index.14a59965.js";const o={},_={type:"button",class:"btn btn-primary"};function c(r,s){return e(),n("button",_,"Primary")}const u=t(o,[["render",c]]);export{u as default};
