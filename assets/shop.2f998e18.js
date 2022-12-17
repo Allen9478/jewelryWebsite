48121c823bfa0f7bd75f0008af48e868aa69282b
Allen <allenyu0708@gmail.com>
2022-12-17 23:51:52 +0800

    keep

---
+++ assets/shop.2f998e18.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index.052276bf.js";const n={},c={type:"button",class:"btn btn-outline-danger"};function s(r,_){return e(),o("button",c,"shop")}const p=t(n,[["render",s]]);export{p as default};
